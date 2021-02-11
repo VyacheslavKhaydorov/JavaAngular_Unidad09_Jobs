ad3f0c11f958f911d082732823392cefa6d97831
Ingrid <1415ingriddominguez@gmail.com>
2021-02-11 17:24:45 +0100

    Método validación

--- M2/src/MainApp.java
+++ M2/src/MainApp.java
@@ -2,8 +2,34 @@
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Empleado empleado1 = new Empleado ("Julio", 1200, "Boss", 400);
+		empleado1.toString();
 
 	}
-
+	
+	//Método
+	public boolean validacion (String nombre, int sueldo, String categoria, Empleado empleado1) {
+		boolean bien = false;
+		if (empleado1.categoria = "Boss" && empleado1.sueldo > 8000) {
+			bien = true;
+		}
+		if (empleado1.categoria = "Manager") && (empleado1.sueldo >= 3000 && empleado1.sueldo < 5000)) {
+			bien = true;
+		}
+		if (empleado1.categoria = "Senior" && (empleado1.sueldo >= 2700 && empleado1.sueldo < 3000)) {
+			bien = true;
+		}
+		if (empleado1.categoria = "Mid" && (empleado1.sueldo >= 1800 && empleado1.sueldo < 2500)) {
+			bien = true;
+		}
+		if (empleado1.categoria = "Junior" && (empleado1.sueldo >= 900 && empleado1.sueldo < 1600)) {
+			bien = true;
+		}
+		if (empleado1.categoria = "Volunteer") {
+			bien = true;
+		}
+		
+		return bien;
+		
+		}
 }
