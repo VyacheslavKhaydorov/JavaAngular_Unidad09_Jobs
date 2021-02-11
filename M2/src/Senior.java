69aeae39cde4e5212946041a7d49402f07a7d648
Ingrid <1415ingriddominguez@gmail.com>
2021-02-11 16:51:24 +0100

    Subclase Senior

---
+++ M2/src/Senior.java
@@ -0,0 +1,10 @@
+
+class Senior extends Empleado{
+	//Atributos
+	
+	//Constructores
+	public Senior(String nombre, int sueldo, String categoria, double reduccion) {
+		super(nombre, sueldo, categoria, reduccion);
+	}
+	
+}
