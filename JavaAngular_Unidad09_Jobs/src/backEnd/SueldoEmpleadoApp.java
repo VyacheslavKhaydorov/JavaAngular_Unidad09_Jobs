7f5db80f8578a3334049780d1b9f320171ff65fd
VyacheslavKhaydorov <78428134+VyacheslavKhaydorov@users.noreply.github.com>
2021-02-12 15:03:04 +0100

    Comentario main

--- JavaAngular_Unidad09_Jobs/src/backEnd/SueldoEmpleadoApp.java
+++ JavaAngular_Unidad09_Jobs/src/backEnd/SueldoEmpleadoApp.java
@@ -15,7 +15,7 @@ public class SueldoEmpleadoApp {
 	
 	public static void main(String[] args) {
 		
-		//Entrada de datos por teclado con Scanner
+		/*//Entrada de datos por teclado con Scanner
 		String entradaDeDatos = "";
 		Scanner input = new Scanner(System.in);
 		System.out.println("Hay bonus de salario para este año? (Y/N)");
@@ -25,7 +25,7 @@ public class SueldoEmpleadoApp {
 		if (entradaDeDatos == "Y") {
 			Empleado tenemosBonus = new Empleado();
 			tenemosBonus.setTenemosBonus(true);
-		}
+		}*/
 		
 		//Mostramos los empleados almacenados en ArchivosEmpleado
 		ArchivoEmpleados.mostramosEmpleados();
